22bf2b3b90c1ce2f5093270c8e7a8768e1db7d92
Jnaneshwar "JD" Das <jnaneshwar.das@gmail.com>
2024-03-27 23:26:16 -0700

    Update test-uxrce-dds-attutude-control-rate.py
    updates to how offboard mode is triggered, arming, and vehicle status checks

--- test-uxrce-dds-attutude-control-rate.py
+++ test-uxrce-dds-attutude-control-rate.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-
+import math
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
-from px4_msgs.msg import OffboardControlMode, VehicleRatesSetpoint, VehicleCommand, VehicleAttitude, VehicleStatus
+from px4_msgs.msg import OffboardControlMode, VehicleRatesSetpoint, VehicleCommand, VehicleStatus, VehicleAttitude
+from time import time
 
 
 class OffboardControl(Node):
-    """Node for controlling a vehicle in offboard mode using attitude rate setpoints."""
+    """Node for controlling a vehicle in offboard mode."""
 
     def __init__(self) -> None:
-        super().__init__('offboard_control_rate')
+        super().__init__('offboard_control_takeoff_and_land')
 
         # Configure QoS profile for publishing and subscribing
         qos_profile = QoSProfile(
@@ -23,33 +24,57 @@ class OffboardControl(Node):
         # Create publishers
         self.offboard_control_mode_publisher = self.create_publisher(
             OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile)
-        self.attitude_rate_setpoint_publisher = self.create_publisher(
-            VehicleRatesSetpoint, '/fmu/in/vehicle_rates_setpoint', qos_profile)
         self.vehicle_command_publisher = self.create_publisher(
             VehicleCommand, '/fmu/in/vehicle_command', qos_profile)
-
-        # Create subscribers
         self.vehicle_attitude_subscriber = self.create_subscription(
             VehicleAttitude, '/fmu/out/vehicle_attitude', self.vehicle_attitude_callback, qos_profile)
+        self.attitude_rate_setpoint_publisher = self.create_publisher(
+            VehicleRatesSetpoint, '/fmu/in/vehicle_rates_setpoint', qos_profile)
+        
+
+        # Create subscribers
         self.vehicle_status_subscriber = self.create_subscription(
             VehicleStatus, '/fmu/out/vehicle_status', self.vehicle_status_callback, qos_profile)
 
         # Initialize variables
+        self.offboard_setpoint_counter = 0
         self.vehicle_attitude = VehicleAttitude()
         self.vehicle_status = VehicleStatus()
+        self.takeoff_height = -5.0
 
         # Create a timer to publish control commands
-        self.timer = self.create_timer(0.05, self.timer_callback)
-
-    def vehicle_attitude_callback(self, vehicle_attitude):
-        """Callback function for vehicle_attitude topic subscriber."""
-        self.vehicle_attitude = vehicle_attitude
+        self.timer = self.create_timer(0.1, self.timer_callback)
 
     def vehicle_status_callback(self, vehicle_status):
         """Callback function for vehicle_status topic subscriber."""
         self.vehicle_status = vehicle_status
 
-    # Methods for arm, disarm, engage_offboard_mode, land remain the same
+    def vehicle_attitude_callback(self, vehicle_attitude):
+        """Callback function for vehicle_attitude topic subscriber."""
+        self.vehicle_attitude = vehicle_attitude
+
+    def arm(self):
+        """Send an arm command to the vehicle."""
+        self.publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=1.0)
+        self.get_logger().info('Arm command sent')
+
+    def disarm(self):
+        """Send a disarm command to the vehicle."""
+        self.publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=0.0)
+        self.get_logger().info('Disarm command sent')
+
+    def engage_offboard_mode(self):
+        """Switch to offboard mode."""
+        self.publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1=1.0, param2=6.0)
+        self.get_logger().info("Switching to offboard mode")
+
+    def land(self):
+        """Switch to land mode."""
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
+        self.get_logger().info("Switching to land mode")
 
     def publish_offboard_control_heartbeat_signal(self):
         """Publish the offboard control mode."""
@@ -59,6 +84,8 @@ class OffboardControl(Node):
         msg.acceleration = False
         msg.attitude = False
         msg.body_rate = True
+        msg.thrust_and_torque = False
+        msg.direct_actuator = False
         msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
         self.offboard_control_mode_publisher.publish(msg)
 
@@ -72,20 +99,47 @@ class OffboardControl(Node):
         msg.thrust_body = [0.0, 0.0, thrust]
         msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
         self.attitude_rate_setpoint_publisher.publish(msg)
-        self.get_logger().info(f"Publishing attitude rate setpoints: Roll rate {roll_rate}, Pitch rate {pitch_rate}, Yaw rate {yaw_rate}, Thrust {thrust}")
+
+    def publish_vehicle_command(self, command, **params) -> None:
+        """Publish a vehicle command."""
+        msg = VehicleCommand()
+        msg.command = command
+        msg.param1 = params.get("param1", 0.0)
+        msg.param2 = params.get("param2", 0.0)
+        msg.param3 = params.get("param3", 0.0)
+        msg.param4 = params.get("param4", 0.0)
+        msg.param5 = params.get("param5", 0.0)
+        msg.param6 = params.get("param6", 0.0)
+        msg.param7 = params.get("param7", 0.0)
+        msg.target_system = 1
+        msg.target_component = 1
+        msg.source_system = 1
+        msg.source_component = 1
+        msg.from_external = True
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.vehicle_command_publisher.publish(msg)
 
     def timer_callback(self) -> None:
         """Callback function for the timer."""
         self.publish_offboard_control_heartbeat_signal()
 
-        # Example of publishing an attitude rate setpoint. Adjust values as needed.
+        if self.offboard_setpoint_counter == 10:
+            self.engage_offboard_mode()
+            self.arm()
+
         if self.vehicle_status.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD:
-            self.publish_attitude_rate_setpoint(0.0, 0.0, 3,0.6)
+            target_yaw_rate = 0.22
+            
+            self.publish_attitude_rate_setpoint(0.0, 0.0, target_yaw_rate, 0.5)
+
+
+        if self.offboard_setpoint_counter < 11:
+            self.offboard_setpoint_counter += 1
 
 
 
 def main(args=None) -> None:
-    print('Starting offboard control node with attitude control...')
+    print('Starting offboard control node...')
     rclpy.init(args=args)
     offboard_control = OffboardControl()
     rclpy.spin(offboard_control)
